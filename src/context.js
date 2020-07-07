3fcdf3da4f2d19e7d49976e76cb848bf69118930
zbadiru <zbadiru@live.co.uk>
2020-07-07 17:48:33 +0100

    day 3 project

--- src/context.js
+++ src/context.js
@@ -6,24 +6,41 @@ const ProductContext = React.createContext()
 // Provider
 // Consumer
 
-class ProductProvider extends Component {
+class ProductProvider extends Component { 
     state = {
-        products: []
-    }
+        products: [],
+        detailsProduct: []
+    } 
 
-    addToCart = () => {
-        console.log('hello from add to cart');
+    getItem = (id) => this.state.products.find(item => item.id === id)
+    
+
+    handleDetails = (id) => {
+        const product = this.getItem(id)
+        this.setState(() =>{
+            return {detailsProduct: product}
+        })
+    }
+    
+    addToCart = (id) => {
+        console.log(`hello from add to cart the is ${id}`);
     }
 
     componentDidMount() {
         fetch("http://localhost:3001/phones")
         .then((data) => data.json())
-        .then((products) => this.setState({ products }))
+        .then((products) => this.setState({
+            products: products,
+            detailsProduct: products[0]
+        }))
+        
+        
     }
     render() {
         return (
             <ProductContext.Provider value={{
                 ...this.state,
+                handleDetails: this.handleDetails,
                 addToCart: this.addToCart
             }}>
                 {this.props.children}
